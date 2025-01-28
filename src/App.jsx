bc8d41ee6d2f1a8248c207bcfe2542ae040e9923
Vaibhav Rai <vaibhavrai974@gmail.com>
2025-01-28 11:16:49 +0530

    Done with the background and also implemented search box

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Search from './components/Search'
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState('i am batwoman')
   return (
-    <div className='text-3xl font-bold underline'>
-      Hello 
-    </div>
+    <main>
+      <div className='pattern'/>
+      <div className='wrapper'>
+        <header>
+          <img src='hero-img.png'></img>
+          <h1>find <span className='text-gradient'>Movie</span> which you like to watch</h1>
+        </header>
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+      </div>
+    </main>
   )
 }
 
